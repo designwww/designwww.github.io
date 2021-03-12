2946f39b1d0abd8ca8291b9437a509135fe52558
designwww <80537910+designwww@users.noreply.github.com>
2021-03-12 19:38:16 +0300

    add site

---
+++ js/script.js
@@ -0,0 +1,39 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const samplesItems = document.querySelectorAll('.samples_item');
+
+    samplesItems.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            item.firstElementChild.classList.toggle('samples_eye_active');
+        })
+    });
+
+    samplesItems.forEach(item => {
+        item.addEventListener('mouseout', () => {
+            item.firstElementChild.classList.toggle('samples_eye_active');
+        })
+    });
+
+    const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu_item'),
+        hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    });
+    $(function () {
+        $("a[href^='#']").click(function () {
+            var _href = $(this).attr("href");
+            $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+            return false;
+        });
+    });
+
+})
